4b29bd6139fe7252b7d040c10f34b6323092aafe
Sem Schilder <sem@tropical.email>
2022-08-21 18:57:29 +0200

    Use and set-up base httpClientBuilder class

--- src/Client.php
+++ src/Client.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace Xvilo\HackerNews;
 
+use Xvilo\HackerNews\HttpClient\HttpClientBuilder;
+
 class Client
 {
+    private HttpClientBuilder $httpClientBuilder;
 
+    public function __construct(HttpClientBuilder $httpClientBuilder = null)
+    {
+        $this->httpClientBuilder = $httpClientBuilder ?: new HttpClientBuilder();
+    }
 }
